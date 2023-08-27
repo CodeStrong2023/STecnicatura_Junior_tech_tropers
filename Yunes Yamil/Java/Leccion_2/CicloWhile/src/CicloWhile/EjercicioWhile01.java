ad4c848cc8d5e9a92230cca25ae5a3bbc994537e
Yamil J. Yunes <yunesyamil@gmail.com>
2023-08-27 17:59:22 -0300

    Clase 1 Java. Ciclo While, DoWhile, For, Break, Continue, Labels

--- Yunes Yamil/Java/Leccion_2/CicloWhile/src/CicloWhile/EjercicioWhile01.java
+++ Yunes Yamil/Java/Leccion_2/CicloWhile/src/CicloWhile/EjercicioWhile01.java
@@ -3,10 +3,49 @@ package CicloWhile;
 
 public class EjercicioWhile01 {
     public static void main(String[] args) {
+        
+        // CICLO WHILE 
+        System.out.println("*** Ciclo While ***");
         var conteo = 0; //inferencia de tipos
-        while(conteo < 7){
+        while(conteo < 3){
             System.out.println("conteo = " + conteo);
             conteo++; //vamos aumentando en uno la variable
         }
+        
+       // CICLO DO - WHILE
+       System.out.println("*** Ciclo Do-While ***");
+
+       var contador = 0;
+       do{
+           System.out.println("contador = " + contador); 
+           contador++;
+       }while(contador <= 7);
+       
+       
+       
+       // CICLO FOR
+       System.out.println("*** Ciclo For ***");
+
+       // Numeros pares
+       
+       System.out.println("* Pares *");
+       inicio:
+       for(var contando = 0; contando < 7; contando++){
+           if(contando % 2 == 0){
+                System.out.println("contando = " + contando);
+                break inicio;
+           }
+       }
+       // Numeros impares
+
+       System.out.println("* Impares *");
+       inicio:
+       for(var contando = 0; contando < 7; contando++){
+           if(contando % 2 != 0){
+               continue inicio; //Vamos a la siguiente iteración
+           }
+                System.out.println("contando = " + contando);
+           }
+       
     }
 }
