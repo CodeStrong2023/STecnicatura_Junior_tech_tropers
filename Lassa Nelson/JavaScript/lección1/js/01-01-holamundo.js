3d1a199682cd013c3589a256023b56165dde30e7
Lassa <nelsonlassa63@gmail.com>
2023-08-28 20:23:54 -0300

    Clase2 javascipt

---
+++ Lassa Nelson/JavaScript/lección1/js/01-01-holamundo.js
@@ -0,0 +1,8 @@
+
+var nombre = "Nelson ";
+var apellido = "Lassa";
+var nombres = "Nelson \nEzequiel\n"
+console.log(nombre);
+console.log(apellido);
+console.log(nombre + apellido);
+console.log(nombres)
