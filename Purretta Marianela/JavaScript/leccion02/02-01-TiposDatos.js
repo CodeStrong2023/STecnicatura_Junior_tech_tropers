9f739b7c8b1b7ea5bf26cb9553163fd903199138
Marianela Purretta <marianela.purretta@yahoo.com>
2023-09-07 17:58:30 -0300

    Agrego Clase2 JS Tipos de Datos

---
+++ Purretta Marianela/JavaScript/leccion02/02-01-TiposDatos.js
@@ -0,0 +1,14 @@
+//Tipos de datos en JS
+//sintáxis para comentarios = Java: //, /*...*/
+var nombre = 'Marianela'; //tipo str
+console.log(nombre);
+
+var numero = 3000; //tipo int
+console.log(numero);
+
+var objeto = {
+    nombre : 'Marianela',
+    apellido : 'Purretta',
+    telefono : '2604641466'
+}
+console.log(objeto);
