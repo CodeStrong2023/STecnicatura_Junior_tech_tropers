84d2032826a0233895c153a988e34b943eb50242
joaquin9830 <joaquin9811@gmail.com>
2023-09-10 15:41:02 -0300

    Clase 2 JavaScript

---
+++ Viñolo Joaquin/JavaScript/Clase 2/TiposDatos.js
@@ -0,0 +1,21 @@
+// Tipos de datos en javascript
+/*
+La sintaxis en lo que es comentarios
+es muy similar a la de Java
+realmente diriamos que es identica
+*/
+var nombre = 'Joaquín';
+console.log(nombre);
+
+var numero = 3000;
+console.log(numero)
+
+var objeto = {
+    nombre : "Joaquín",
+    apellido : "Viñolo",
+    DNI : "4234324",
+    telefono : "243234325498"
+
+}
+
+console.log(objeto);
