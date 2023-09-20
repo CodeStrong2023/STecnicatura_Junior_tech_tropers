536a19fcc40f86af372ab68664b49429fe052c1b
Franco <francopaganoo2015@gmail.com>
2023-09-20 06:18:50 -0300

    Clase JS3 - Tipo de datos (comentarios, tipos de datos y Ejecucion en navegador)

---
+++ Pagano Franco/JavaScript/Leccion 2/02-01-Tipos de datos en JavaScript.js
@@ -0,0 +1,18 @@
+// Tipos de datos en JavaScript    
+/*
+La sintaxis en lo que es comentarios es muy
+similar a la de Java realmente diria que es
+identica
+*/
+var nombre = "Franco"; //Tipo Str
+console.log(nombre);
+
+var numero = 3000; //Tipo numerico
+console.log(numero);
+
+var objeto = {
+    nombre : "Franco",
+    apellido : "Pagano",
+    telefono: "2604694746"
+}
+console.log(objeto);
