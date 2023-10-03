df5373d0bf560eb8d3cf7a668332af2b1fa0d839
Franco <francopaganoo2015@gmail.com>
2023-10-02 22:05:56 -0300

    Clase JS7 Simitules entre Java y JavaScript - parte 2, parte 3 y Ejercicios

---
+++ Pagano Franco/JavaScript/Leccion 3/03-01OperadoresJavaScript.js
@@ -0,0 +1,18 @@
+//Ejercicio para encontrar numeros pares
+
+let numero = 10;
+if (numero % 2 == 0) {
+    console.log("Es un numero PAR");
+}
+else {
+    console.log("Es un numero IMPAR");
+}
+
+//Ejercicio: es mayor de edad
+let edad = 20;
+if (edad > 18){
+    console.log("Es mayor de edad ");
+}
+else{
+    console.log("Es menor de edad");
+}
