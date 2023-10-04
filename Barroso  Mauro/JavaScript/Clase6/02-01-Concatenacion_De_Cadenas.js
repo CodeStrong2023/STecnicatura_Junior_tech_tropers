c534c807f0f35e8561307a43636f99b5fa9d001a
Mauro <maurobarroso1999@gmail.com>
2023-10-04 20:16:15 -0300

    Clase 6 y 7 JavaScript

---
+++ Barroso  Mauro/JavaScript/Clase6/02-01-Concatenacion_De_Cadenas.js
@@ -0,0 +1,34 @@
+var nombre = "Mauro";
+var apellido = " Barroso";
+var nombreCompleto = nombre+" "+apellido;
+console.log(nombreCompleto);
+var nombreCompleto2 = "Danilo "+ " " +"Munoz";
+console.log(nombreCompleto2); 
+var juntos = nombre + 219;
+console.log(juntos);
+juntos = nombre + 78 + 17;
+console.log(juntos);
+juntos = 78 + 17 + nombre;
+console.log(juntos);
+
+nombre += apellido;
+console.log(nombre);
+
+//Ya no se usa var, sino que se utiliza let y const
+let nombre2 = "Pedro";
+console.log(nombre2);
+
+const apellido2 = "Lepes"
+//apellido2 = "Perez"; una constante no puede ser modificada
+console.log(apellido2)
+
+Let x, y; 
+x = 17, y = 21;
+Let z = x + y;
+console.log(z)
+Let _1num = 31;
+Let $break = "rompe";
+
+console.log (_1num)
+console.log ($break)
+
