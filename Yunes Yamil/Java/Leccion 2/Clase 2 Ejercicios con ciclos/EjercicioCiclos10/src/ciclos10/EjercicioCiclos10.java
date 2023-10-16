337284875ab5edc958bec475851cb0577385fa4e
Yamil J. Yunes <yunesyamil@gmail.com>
2023-10-16 19:14:55 -0300

    Java Clase 6 Ejercicio 10 Ciclos

---
+++ Yunes Yamil/Java/Leccion 2/Clase 2 Ejercicios con ciclos/EjercicioCiclos10/src/ciclos10/EjercicioCiclos10.java
@@ -0,0 +1,20 @@
+/*
+Ejercicio 10:
+Pedir 10 numeros y escribir la suma total
+Hacerlo con la clase Scanner y JOptionPane
+*/
+package ciclos10;
+
+import javax.swing.JOptionPane;
+
+public class EjercicioCiclos10 {
+    public static void main(String[] args) {
+        
+        int numero, suma = 0;
+        for(int i=1; i<=10; i++){
+            numero = Integer.parseInt(JOptionPane.showInputDialog("Digite un numero: "));
+            suma += numero;
+        }
+        JOptionPane.showMessageDialog(null, "\nLa suma de todos los numeros es: "+suma);
+    } 
+}
