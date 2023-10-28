97ea8b6b28bd8ff7bba785a827166dda88df3639
joaquin9830 <joaquin9811@gmail.com>
2023-10-28 15:04:42 -0300

    Clase 7 JavaScript

---
+++ Viñolo Joaquin/JavaScript/Clase 7/ConcatenacionDeCadenas.js
@@ -0,0 +1,33 @@
+let nombre = "Joaquín";
+let apellido = "Viñolo";
+let nombreCompleto = nombre + " " + apellido;
+console.log(nombreCompleto);
+let nombreCompleto2 = "Joaquín" + " " + "Viñolo";
+console.log(nombreCompleto2);
+let juntos = nombre + 219; //Lee de izquierda a derecha siguienda la cadena, lee el numero como string
+console.log(juntos);
+juntos = nombre + 78 + 17;
+console.log(juntos);
+juntos = nombre + (78 + 17);
+console.log(juntos);
+
+nombre += " " + apellido;
+console.log(nombre);
+
+//Hoy ya no se usa var, usamos let y const
+let nombre2 = "Joaquín";
+console.log(nombre2);
+
+
+const apellido2 = "Viñolo";
+console.log(apellido2);
+
+let x, y; //Se pueden crear varias vaiables en una misma línea
+x = 17, y = 21; //Asignación de varias variables en la misma línea
+let z = x + y; //Se le asigna a la variable el valor de la asignación
+console.log(z);
+
+let _1num = 31; //No se puede usar un número al principio del nombre de una variable
+//let break = "romer"; //No se puede usar break como nombre de variable
+
+console.log(_1num);
