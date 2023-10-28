a1f7befd0d2a20034271260602ae3859dcda9ec7
joaquin9830 <joaquin9811@gmail.com>
2023-10-28 15:34:34 -0300

    Clase 8 JavaScript

---
+++ Viñolo Joaquin/JavaScript/Clase 8/OperadoresJavaScript.js
@@ -0,0 +1,25 @@
+//Ejercicio para encontrar numeros pares 
+let parImpar = 10;
+if(parImpar % 2 == 0){
+    console.log("El numero es par");
+}else{
+    console.log("El numero es impar");
+}
+
+//Ejercicio es mayor de edad
+let edad = 20, adulto = 18;
+if(edad >= adulto){
+    console.log("Es mayor de edad");
+}else{
+    console.log("Es menor de edad");
+}
+
+//Ejercicio: Dentro de un rango
+let dentroRango = 5;
+let valMin = 0, valMax = 10;
+
+if(dentroRango >= valMin && dentroRango <= valMax){
+    console.log("Dentro de rango");
+}else{
+    console.log("Fuera de rango");
+}
