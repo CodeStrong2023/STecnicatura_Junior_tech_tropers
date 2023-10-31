7b2d1280fbdeb22b24a05ff3dd93024b06da3b08
Marianela Purretta <marianela.purretta@yahoo.com>
2023-10-31 14:17:49 -0300

    Clase 10 PYTHON POO parte2

--- Purretta Marianela/Python/Clase10/Leccion6/Persona2.py
+++ Purretta Marianela/Python/Clase10/Leccion6/Persona2.py
@@ -32,44 +32,51 @@ class Persona2:
     def edad(self, edad):
         self._edad = edad
 
-persona1 = Persona2('Marianela', 'Purretta', 37)
-print(persona1.nombre) #Llamamos al método getter
-print(persona1.apellido)
-print(persona1.edad)
-persona1.nombre = 'Sebastian' # otra manera de obtener método set
-print(persona1.nombre)# llamamos al método getter
-print(persona1.mostrar_detalles()) # llamamos al método mostrar_detalles
-#Atributo read-only, la edad no tiene método set
-print(persona1.edad)
+    #MÉTODO DESTRUCTOR:
+    def __del__(self):
+        print(f'Persona2: {self._nombre} {self._apellido} {self._edad}')
 
-#TAREA: CREAR TRES OBJETOS MÁS USANDO LOS MÉTODOS getter & setter PARA MODIFICAR Y MOSTRAR LOS CAMBIOS
+if __name__ =='__main__':
+    persona1 = Persona2('Marianela', 'Purretta', 37)
+    print(persona1.nombre) #Llamamos al método getter
+    print(persona1.apellido)
+    print(persona1.edad)
+    persona1.nombre = 'Sebastian' # otra manera de obtener método set
+    print(persona1.nombre)# llamamos al método getter
+    print(persona1.mostrar_detalles()) # llamamos al método mostrar_detalles
+    #Atributo read-only, la edad no tiene método set
+    print(persona1.edad)
 
-persona2 = Persona2('Adriel Emir', 'Siri', 13)
-print(persona2.mostrar_detalles())
-persona2.nombre = 'Sahir'
-persona2.apellido = 'Siria'
-persona2.edad = 1
-print(persona2.nombre)
-print(persona2.apellido)
-print(persona2.edad)
-print(persona2.mostrar_detalles())
+    #TAREA: CREAR TRES OBJETOS MÁS USANDO LOS MÉTODOS getter & setter PARA MODIFICAR Y MOSTRAR LOS CAMBIOS
 
-persona3 = Persona2('Nassira', 'Siri', 6)
-print(persona3.mostrar_detalles())
-persona3.nombre = 'Abby'
-persona3.apellido = 'Abdul'
-persona3.edad = 35
-print(persona3.nombre)
-print(persona3.apellido)
-print(persona3.edad)
-print(persona3.mostrar_detalles())
+    persona2 = Persona2('Adriel Emir', 'Siri', 13)
+    print(persona2.mostrar_detalles())
+    persona2.nombre = 'Sahir'
+    persona2.apellido = 'Siria'
+    persona2.edad = 1
+    print(persona2.nombre)
+    print(persona2.apellido)
+    print(persona2.edad)
+    print(persona2.mostrar_detalles())
 
-persona4 = Persona2('Abigail', 'Yusuf', 24)
-print(persona4.mostrar_detalles())
-persona4.nombre = 'Cristian'
-persona4.apellido = 'Yunes'
-persona4.edad = 48
-print(persona4.nombre)
-print(persona4.apellido)
-print(persona4.edad)
-print(persona4.mostrar_detalles())
+    persona3 = Persona2('Nassira', 'Siri', 6)
+    print(persona3.mostrar_detalles())
+    persona3.nombre = 'Abby'
+    persona3.apellido = 'Abdul'
+    persona3.edad = 35
+    print(persona3.nombre)
+    print(persona3.apellido)
+    print(persona3.edad)
+    print(persona3.mostrar_detalles())
+
+    persona4 = Persona2('Abigail', 'Yusuf', 24)
+    print(persona4.mostrar_detalles())
+    persona4.nombre = 'Cristian'
+    persona4.apellido = 'Yunes'
+    persona4.edad = 48
+    print(persona4.nombre)
+    print(persona4.apellido)
+    print(persona4.edad)
+    print(persona4.mostrar_detalles())
+
+    print(__name__)
--- Purretta Marianela/Python/Clase10/Leccion6/PruebaPersona2.py
+++ Purretta Marianela/Python/Clase10/Leccion6/PruebaPersona2.py
@@ -1,5 +1,10 @@
 from Persona2 import Persona2 #importamos la clase Persona2
+print('Creación de objetos'.center(50, '-'))
+if __name__ == '__main__':
+    persona5 = Persona2('Lucía', 'Arauz', 54)
+    persona5.mostrar_detalles() #Muestra todos los objetos y dela clase
 
-persona5 = Persona2('Lucía', 'Arauz', 54)
-persona5.mostrar_detalles() #Muestra todos los objetos y dela clase
+    print(__name__) #para ver hasta donde se esta ejecutando un código u otro
 
+print('Eliminación de objetos'.center(50,'-'))# 50 y - es para centrar la cadena
+del persona5
