495a73615ef78ec9f19af451a2f22587b1e247ac
Yamil J. Yunes <yunesyamil@gmail.com>
2023-10-31 00:16:53 -0300

    Clase 4 Javascript

---
+++ Yunes Yamil/JavaScript/Clase 4/TiposDatos.js
@@ -0,0 +1,40 @@
+let nombre = "Yamil Jesus";
+//Tipo string
+console.log(typeof nombre);
+num = 7;
+console.log(typeof num);
+//Tipo numerico
+let numero = 3000;
+console.log(typeof numero);
+
+//Tipo objeto
+let objeto = {
+    nombre:"Yamil Jesus",
+    apellido:"Yunes",
+    edad: 29
+}
+
+console.log(typeof objeto);
+
+//Tipos de datos booleanos
+let bandera = true;
+console.log(bandera);
+console.log(typeof bandera);
+
+//Tipos de datos function
+function miFuncion(){}
+console.log(typeof miFuncion)
+
+//Tipo de dato Symbol
+let simbolo  = Symbol("Mi simbolo");
+console.log(simbolo);
+
+//Tipo de dato clase 
+class Persona{
+    constructor(nombre, apellido){
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+}
+
+console.log(typeof Persona);
