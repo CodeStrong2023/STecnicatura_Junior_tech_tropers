a6453cc4e5efa20bc0e99266b6d86d51e1d5c4dc
Franco <francopaganoo2015@gmail.com>
2023-11-04 00:40:39 -0300

    Clase 9 Algunos operadores y Algunas funciones - Ejercicios - funcion number - funcion isNan

--- Pagano Franco/JavaScript/Leccion 2/02-01-TiposDatos.js
+++ Pagano Franco/JavaScript/Leccion 2/02-01-TiposDatos.js
@@ -18,3 +18,24 @@ console.log(nombre);
 nombre = "Franco " //Podemos agregar un espacio en el nonbre a la hora de concatenar
 nombre += apellido; 
 console.log(nombre);
+
+let x, y; //Se pueden crear varios variables dentro de una misma lista
+x = 17, y = 21; //Se puede hacer varias asignaciones de varias variables dentor de la misma linea
+let z = x + y;
+console.log(z);
+
+let $1num = 31; //El signo $  habilita colocar numeros, pero no es recomendable
+let _1num = 31;
+console.log(_1num)
+
+// let break = "rompe"; No usar palabras reservadas en variables
+let _break = "rompe";
+console.log(_break)
+
+//Hoy ya no se usa var, se utiliza let y const
+let nombre2 = "Franco";
+console.log(nombre2);
+
+const apellido2 = "Pagano";
+//apellido2 = "Pantaley"; //una constante no puede ser modificada
+console.log(apellido2);
--- Pagano Franco/JavaScript/Leccion 3/03-01OperadoresJavaScript.js
+++ Pagano Franco/JavaScript/Leccion 3/03-01OperadoresJavaScript.js
@@ -16,3 +16,52 @@ if (edad > 18){
 else{
     console.log("Es menor de edad");
 }
+
+// Ejercicio: dentro de un rango
+let dentroRango = 5; //Aqui vamos a ir cambiando el valor
+let valMin = 0, valMax = 10;
+if(dentroRango >= valMin && dentroRango <= valMax){
+    console.log("Esta adentro del rango establecido");
+} else {
+    console.log("Esta fuera del rango establecido");
+}
+
+//Ejercicio: si el padre puede asisitir al juego de su hijo
+let vacacuines = false, diaDescanso = false;
+if(vacaciones || diaDescanso){
+    console.log("El padre puede asistir al juego de su hijo")
+}
+else{
+    console.log("El padre no puede asistir al juego de su hijo")
+}
+
+//Operador ternario
+let resultado2 = 3 > 2 ? "Verdadero" : "False"
+console.log(resultado2);
+let numero1 = 12;
+resultado2 = numero2 % 2 == 0 ? "Es un numero PAR" : "Es un numero IMPAR"
+console.log(resultado2);
+
+
+//Convertir String a numero
+let miNumero = "10";
+console.log(typeof miNumero);
+let edad2 = Number(miNumero);
+console.log(typeof edad2);
+if(isNaN(edad2)){ //No es un numero = is not a number
+    console.log("Esta variable no contiene solo numeros")
+}
+else {
+    if(edad2 >= 18){
+        console.log("Ouede votar");
+    } 
+    else {
+        console.log("Muy joven para votar");
+    }
+}
+
+let resultado3 = edad2 >= 18 ? "Puede votar" : "Muy joven para votar";
+console.log(resultado3);
+
+//Funcion isNaN
+
