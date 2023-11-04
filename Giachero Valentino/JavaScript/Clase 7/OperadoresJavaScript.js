a9cd8b7b9434115d019e8173615d41a364a67c01
Valentino <jostar843@gmail.com>
2023-11-04 00:25:41 -0300

    Javascript clase 7

---
+++ Giachero Valentino/JavaScript/Clase 7/OperadoresJavaScript.js
@@ -0,0 +1,15 @@
+//Ejercicio para encontrar numeros pares 
+let parImpar = 10;
+if(parImpar % 2 == 0){
+    console.log("El numero es par");
+}else{
+    console.log("El numero es impar");
+}
+
+//Ejercicio es mayor de edad
+let edad = 20, adulto = 18;
+if(edad >= adulto){
+    console.log("Es mayor de edad");
+}else{
+    console.log("Es menor de edad");
+}
