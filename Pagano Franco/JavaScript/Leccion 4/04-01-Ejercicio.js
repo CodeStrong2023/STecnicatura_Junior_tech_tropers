69bf8f4411b18fde9eacc033b05b8a5e21a302e3
Franco <francopaganoo2015@gmail.com>
2023-11-06 09:52:34 -0300

    Clase 10 Ejercicios - Calcular estacion del año - Hora del dia - Estructura Switch con breack

--- Pagano Franco/JavaScript/Leccion 4/04-01-Ejercicio.js
+++ Pagano Franco/JavaScript/Leccion 4/04-01-Ejercicio.js
@@ -50,3 +50,55 @@ console.log(fechaNacimiento);
 fechaNacimiento = 1996;
 console.log(fechaNacimiento); //Solo se ejecuta la consola anterior
 
+// Clase 10 
+//Ejercicio 1: Calcular estacion del año
+let mes = 4;
+let estacion; 
+if (mes == 1 || mes == 2 || mes == 3){
+    estacion = "Verano";
+} else if (mes == 3 || mes == 4 || mes == 5){
+    estacion == "Otoño";
+} else if(mes == 6 || mes == 7 || mes == 8){
+    estacion = "Invierno";
+} else if (mes == 9 || mes == 10 || mes == 11){
+    estacion  == "Primavera"
+} else {
+    estacion = "Valor incorrecto"
+}
+console.log(estacion);
+
+switch(mes){
+    case 1: case 2: case 12:
+        estacion = "VERANO";
+        break;
+    case 3: case 4: case 5:
+        estacion = "OTOÑO";
+        break;
+    case 6: case 7: case 8:
+        estacion = "INVIERNO";
+        break;
+    case 9: case 10: case 11:
+        estacion = "PRIMAVERA";
+        break;
+    default:
+        estacion = "Valor incorrecto";
+    }
+
+    console.log(estacion);
+
+//Ejercicio 2: Hora del dia 
+let hora = 8;
+let mensaje; 
+if(hora >= 6 && hora <= 11){
+    mensaje == "MAÑANA";
+} else if (hora >= 12 && hora <= 18){
+    mensaje == "TARDE";
+} else if (hora >= 18 && hora <= 21){
+    mensaje ="NOCHE";
+} else if (hora >= 21 && hora <= 23){
+    mensaje = "DORMIR"
+} else {
+    mensaje = "Valor incorrecto"
+}
+
+console.log(mensaje);
