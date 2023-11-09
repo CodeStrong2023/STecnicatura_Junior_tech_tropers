d132af3831d81baa46399d4d9813a38e5e26c464
Valentino <jostar843@gmail.com>
2023-11-09 18:05:30 -0300

    Clase 10 Python

---
+++ Giachero Valentino/Python/Clase 10/Aritmetica.py
@@ -0,0 +1,34 @@
+class Aritmetica:
+    """
+    El nombre de este tipo de comentario es: DocString
+    esto es documentación de la clase en python
+    Vamos a hacer en esta clase algunas operaciones de: suma,
+    resta, multiplicación y más
+    """
+    def __init__(self, operandoA, operandoB):
+        self.operandoA = operandoA
+        self.operandoB = operandoB
+
+    #Método para sumar
+    def sumar(self):
+        return self.operandoA + self.operandoB
+
+    #Método para restar:
+    def resta(self):
+        return self.operandoA - self.operandoB
+
+    #Método para multiplicar
+    def multiplicar(self):
+        return self.operandoA * self.operandoB
+
+    #Método para dividir:
+    def dividir(self):
+        return self.operandoA / self.operandoB
+
+#Creamos una instancia de la clase aritmetica
+aritmetica1 = Aritmetica(7,9) #Le pasamos los argumentos para los operandos
+print(aritmetica1.sumar())
+
+print(f"La resta de los números es: {aritmetica1.resta()}")
+print(f"La multiplicación de los números es: {aritmetica1.multiplicar()}")
+print(f"La división de los números es: {aritmetica1.dividir():.2f}")
---
+++ Giachero Valentino/Python/Clase 10/Persona2.py
@@ -0,0 +1,88 @@
+class Persona2:
+    def __init__(self, nombre, apellido, edad):  # Esta encapsulado
+        self._nombre = nombre
+        self._apellido = apellido
+        self._edad = edad
+
+    def mostrar_detalles(self):
+        print(f'Los datos a mostrar son los siguientes: {self._nombre} {self._apellido} {self._edad}')
+
+    @property  # Decorador
+    def nombre(self):  # Método Getter
+        print('Estamos utilizando método get')
+        return self._nombre
+
+    @nombre.setter
+    def nombre(self, nombre):  # Método setter
+        print("Estamos utilizando el método set")
+        self._nombre = nombre
+
+    # Tarea definir los getter y setter de apellido y edad
+
+    # Getter y setter del atributo apellido
+    @property
+    def apellido(self):
+        print("Estamos utilizando un método get")
+        return self._apellido
+
+    @apellido.setter
+    def apellido(self, apellido):
+        print("Estamos utilizando el método set")
+        self._apellido = apellido
+
+    # Getter y setter del atributo edad
+    @property
+    def edad(self):
+        print("Estamos utilizando un método get")
+        return self._edad
+
+    @edad.setter
+    def edad(self, edad):
+        print("Estamos utilizando el método set")
+        self._edad = edad
+
+    def __del__(self):
+        print(f'Persona2: {self._nombre} {self._apellido} {self._edad}')
+
+if __name__ == ' main ':
+    persona1 = Persona2("Juan", "Lopez", 41)
+    print(persona1.nombre)  # Llamamos al método getter
+    persona1.nombre = "Sergio Dennis"  # Llamamos el método setter
+    print(persona1.nombre)
+    print(persona1.mostrar_detalles())  # Llamamos el método mostrar_detalles
+    # Atributo read-only sería la edad porque no tiene el método set
+    print(persona1.edad)
+
+    # Tarea: Crear tres objetos más
+    persona2 = Persona2("Valentino", "Giachero", 21)
+    persona3 = Persona2("Santos", "Randazzo", 65)
+    persona4 = Persona2("Carlos", "Giachero", 58)
+
+    # Objeto 1
+    persona2.nombre = "Valentino Estrella"
+    persona2.apellido = "Giachero"
+    persona2.edad = 21
+    print(persona2.nombre)
+    print(persona2.apellido)
+    print(persona2.edad)
+    persona2.mostrar_detalles()
+
+    # Objeto 2
+    persona3.nombre = "Santos"
+    persona3.apellido = "Randazzo"
+    persona3.edad = 65
+    print(persona3.nombre)
+    print(persona3.apellido)
+    print(persona3.edad)
+    persona3.mostrar_detalles()
+
+    # Objeto 3
+    persona4.nombre = "Carlos"
+    persona4.apellido = "Giachero"
+    persona4.edad = 58
+    print(persona3.nombre)
+    print(persona3.apellido)
+    print(persona3.edad)
+    persona4.mostrar_detalles()
+
+print(__name__)
